f5a216e38da926dd2d3b952f1230b99c5d9f6132
Myktybek7 <myktybekabdykaiymov@gmail.com>
2024-12-05 15:17:39 +0600

    Add password strength assessment function

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
 import React, { useState } from 'react';
 import './App.css';
 
+const assessPasswordStrength = (password) => {
+  let score = 0;
+  if (password.length >= 8) score += 1;
+  if (/[A-Z]/.test(password)) score += 1;
+  if (/[a-z]/.test(password)) score += 1;
+  if (/[0-9]/.test(password)) score += 1;
+  if (/[@#$%^&+=!,.?]/.test(password)) score += 1;
+
+  if (score === 5) return 'Очень надежный';
+  if (score === 4) return 'Надежный';
+  if (score === 3) return 'Удовлетворительный';
+  return 'Слабый';
+};
+
+
 const PasswordGenerator = () => {
   const [password, setPassword] = useState('');
   const [strength, setStrength] = useState('');
